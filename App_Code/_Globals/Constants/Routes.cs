c4a2571f220fe7a04bb0a3701880b6d9efadbb6a
Ken Napura <ken_napura@msi-ecs.com.ph>
2025-04-03 11:47:21 +0800

    Created Partials for SPModalCreation, SPRow, InputFields Implemented SP Table in SP page Implemented Submission of SPCreation in client side

--- App_Code/_Globals/Constants/Routes.cs
+++ App_Code/_Globals/Constants/Routes.cs
@@ -8,6 +8,16 @@
 			public const string OUTLINE_ALERT = @"../Shared/Alerts/_OutlineAlertPartial";
 			public const string SIDEBAR = @"../Shared/_SideBarPartial";
 			public const string NAVBAR = @"../Shared/_NavBarPartial";
+
+			public static class Inputs
+			{
+				public const string TEXTBOX = @"../Shared/Inputs/_InputFieldPartial";
+			}
+			public static class SP
+			{
+				public const string TROW = @"../Shared/Tables/_SPTableRowPartial";
+				public const string MODAL_CREATE = @"../Shared/Modals/_SPModalCreatePartial";
+			}
 		}
 	}
 }
