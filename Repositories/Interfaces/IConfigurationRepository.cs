231958d0d9194578d18128f17a7cbc6f377467ff
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-04-04 09:56:39 +0800

    fix: Correct spelling mistakes in codebase
    - Fixed typos from Provines to Provinces

--- Repositories/Interfaces/IConfigurationRepository.cs
+++ Repositories/Interfaces/IConfigurationRepository.cs
@@ -5,7 +5,7 @@ namespace CCIMS.Web.Repositories.Interfaces
 	public interface IConfigurationRepository
 	{
 		string GetPSGCBaseUrl();
-		string GetPSGCProvines();
+		string GetPSGCProvinces();
 		string GetPSGCCitiesByProvinceCode(string code);
 		string GetPSGCBarangaysByCityCode(string code);
 		ISysAdminSecurityDetails GetSysAdminPrivateDetails();
