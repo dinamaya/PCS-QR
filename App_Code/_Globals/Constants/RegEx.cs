0faa540446798e489f2777774315e0bb6dacb8be
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-05-05 13:45:06 +0800

    fix: Validation for Email to require Top Level Domain

--- App_Code/_Globals/Constants/RegEx.cs
+++ App_Code/_Globals/Constants/RegEx.cs
@@ -12,9 +12,10 @@ namespace CCIMS.Web.App_Code._Globals.Constants
     [StringSyntax(StringSyntaxAttribute.Regex)] public const string SERIALNUMBER = @"[A-Z0-9\-]{6,}";
     [StringSyntax(StringSyntaxAttribute.Regex)] public const string NAMES = @"^[\p{L}\s_.,ñÑ]+$";
     [StringSyntax(StringSyntaxAttribute.Regex)] public const string USERNAME = @"^[\p{L}0-9_.]+$";
-    [StringSyntax(StringSyntaxAttribute.Regex)] public const string EMAIL_LOCAL = @"^[a-zA-Z0-9._%+-]+(?=@)";
+	[StringSyntax(StringSyntaxAttribute.Regex)] public const string PHONE = @"^\d+$";
+	[StringSyntax(StringSyntaxAttribute.Regex)] public const string EMAIL_LOCAL = @"^[\w\.\-]+@[\w\-]+(\.[\w\-]+)*\.[a-zA-Z]{2,}$";
 
-    public static class Characters
+		public static class Characters
     {
       public const string SERIALNUMBER = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._";
     }
