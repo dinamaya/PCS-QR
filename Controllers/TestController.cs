706513d4eb5404244b6bb32b2a7b3d31c5aec6f5
Ken Napura <ken_napura@msi-ecs.com.ph>
2025-05-23 10:29:28 +0800

    feature: - created case email View and send email endpoint for testing and design the email

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CCIMS.Web.App_Code._Globals.Constants;
+using CCIMS.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CCIMS.Web.Controllers
 {
   public class TestController : Controller
   {
-    public IActionResult OCR()
+    public IActionResult CaseEmail()
     {
       return View();
     }
