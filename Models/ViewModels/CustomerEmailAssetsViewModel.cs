c0520f07f6a8f788f33422072feefb4c30bab8ec
Ken Napura <ken_napura@msi-ecs.com.ph>
2025-05-28 11:45:28 +0800

    refactor: - Installed Scriban Library - Convert cshtml to scriban file - Created Email Attachment Complex object - Modified the CustomerEmailAssetsViewModel to remove the unwanted image path and use the content Id instead - Added Base URL in appsettings and fetched it in Config Repo - Added RenderEmailAsync - Implemented Resources linker in Email to link Graphics for the Email Body - Modified Test Send Case Creation Email

--- Models/ViewModels/CustomerEmailAssetsViewModel.cs
+++ Models/ViewModels/CustomerEmailAssetsViewModel.cs
@@ -1,34 +1,24 @@
 ﻿using CCIMS.Web.App_Code._Globals;
+using CCIMS.Web.Repositories.Interfaces;
 using Microsoft.AspNetCore.Hosting.Server;
 
 namespace CCIMS.Web.Models.ViewModels
 {
   public class CustomerEmailAssetsViewModel : CustomerEmailDetailsViewModel
   {
-    private readonly Uri imgFolder;
-    private readonly Uri icnFolder;
-    public Uri BaseUrl { get; }
-    public string WebIcon { get; }
-    public string VstIcon { get; }
-    public string CheckIcon { get; }
-    public string CharacterImage { get; }
+    public string BaseUrl { get; }
     public string CaseTrackingLink { get; }
 
-    public CustomerEmailAssetsViewModel(Server server, CustomerEmailDetailsViewModel emailDetails)
+    public CustomerEmailAssetsViewModel(IConfigurationRepository configRepo, CustomerEmailDetailsViewModel emailDetails)
     {
-      BaseUrl = server.BaseUrl;
-      imgFolder = new Uri(BaseUrl, "img/");
-      icnFolder = new Uri(imgFolder, "icons/");
-      WebIcon = new Uri(icnFolder, "icon_ccims_lg.svg").AbsoluteUri;
-      VstIcon = new Uri(icnFolder, "VST-ECS.png").AbsoluteUri;
-      CheckIcon = new Uri(icnFolder, "circle-check-solid.png").AbsoluteUri;
-      CharacterImage = new Uri(imgFolder, "illustrations/thank-you1.png").AbsoluteUri;
-      CaseTrackingLink = new Uri(BaseUrl, $"Cases/Tracking?refNo={emailDetails.CaseNumber}").AbsoluteUri;
-
       base.Email = emailDetails.Email;
       base.CaseNumber = emailDetails.CaseNumber;
       base.ServicePartner = emailDetails.ServicePartner;
       base.Fullname = emailDetails.Fullname;
+
+      var _uri = new Uri(configRepo.GetBaseUrl());
+      BaseUrl = _uri.AbsoluteUri;
+      CaseTrackingLink = new Uri(_uri, $"Cases/Tracking?refNo={CaseNumber}").AbsoluteUri;
     }
   }
 }
