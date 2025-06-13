f0bf41af66034fbfd55a24f2ed556067f1eb4b76
Ken Napura <KNapura@msi-ecs.com.ph>
2025-06-13 15:08:33 +0800

    refactor: Merged devAldin's Changes

--- Models/SQLViews/Main/WeeklyClosedCasesChartV.cs
+++ Models/SQLViews/Main/WeeklyClosedCasesChartV.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 
 namespace CCIMS.Web.Models.SQLViews.Main;
 
-public partial class WeeklyAgingCasesChartV
+public partial class WeeklyClosedCasesChartV
 {
     public DateOnly? CaseDate { get; set; }
 
     public string? DayOfWeek { get; set; }
 
-    public int TotalAgingCases { get; set; }
+    public int TotalClosedCases { get; set; }
 }
--- Repositories/Implementations/DashboardRepository.cs
+++ Repositories/Implementations/DashboardRepository.cs
@@ -10,39 +10,39 @@ using System.Threading.Tasks;
 
 namespace CCIMS.Web.Repositories
 {
-	public class DashboardRepository : IDashboardRepository
-	{
-		private readonly MainDbContext _context;
-		private readonly ILogger<DashboardRepository> _logger;
+  public class DashboardRepository : IDashboardRepository
+  {
+    private readonly MainDbContext _context;
+    private readonly ILogger<DashboardRepository> _logger;
 
-		public DashboardRepository(MainDbContext context, ILogger<DashboardRepository> logger)
-		{
-			_context = context;
-			_logger = logger;
-		}
-
-		public async Task<int> GetAgingCasesCountAsync() => await _context.AgingCasesVs.CountAsync();
-
-		public async Task<int> GetClosedCasesCountAsync() => await _context.ClosedCasesVs.CountAsync();
-
-		public async Task<int> GetCasesCreatedTodayCountAsync() => await _context.CasesCreatedTodayVs.CountAsync();
-
-		public async Task<int> GetTotalCasesCountAsync() => await _context.Cases.CountAsync();
-
-		public async Task<List<WeeklyCasesChartV>> GetWeeklyCasesChartAsync() =>
-			await _context.WeeklyCasesChartVs.ToListAsync();
-
-		public async Task<List<WeeklyAgingCasesChartV>> GetWeeklyAgingCasesChartAsync() =>
-			await _context.WeeklyAgingCasesChartVs.ToListAsync();
-
-		public async Task<List<LatestCasesSubmissionV>> GetLatestCaseSubmissionsAsync() =>
-			await _context.LatestCasesSubmissionVs.ToListAsync();
-
-		public async Task<List<Top3ServicePartnersAgingCasesV>> GetTop3ServicePartnersAgingCases() =>
-			await _context.Top3ServicePartnersAgingCasesVs.ToListAsync();
-
-        public async Task<List<TopAgingCasesV>> GetTopAgingCases() =>
-            await _context.TopAgingCasesVs.ToListAsync();
+    public DashboardRepository(MainDbContext context, ILogger<DashboardRepository> logger)
+    {
+      _context = context;
+      _logger = logger;
     }
+
+    public async Task<int> GetAgingCasesCountAsync() => await _context.AgingCasesVs.CountAsync();
+
+    public async Task<int> GetClosedCasesCountAsync() => await _context.ClosedCasesVs.CountAsync();
+
+    public async Task<int> GetCasesCreatedTodayCountAsync() => await _context.CasesCreatedTodayVs.CountAsync();
+
+    public async Task<int> GetTotalCasesCountAsync() => await _context.Cases.CountAsync();
+
+    public async Task<List<WeeklyCasesChartV>> GetWeeklyCasesChartAsync() =>
+      await _context.WeeklyCasesChartVs.ToListAsync();
+
+    public async Task<List<WeeklyClosedCasesChartV>> GetWeeklyClosedCasesChartAsync() =>
+      await _context.WeeklyClosedCasesChartVs.ToListAsync();
+
+    public async Task<List<LatestCasesSubmissionV>> GetLatestCaseSubmissionsAsync() =>
+      await _context.LatestCasesSubmissionVs.ToListAsync();
+
+    public async Task<List<Top3ServicePartnersAgingCasesV>> GetTop3ServicePartnersAgingCases() =>
+      await _context.Top3ServicePartnersAgingCasesVs.ToListAsync();
+
+    public async Task<List<TopAgingCasesV>> GetTopAgingCases() =>
+        await _context.TopAgingCasesVs.ToListAsync();
+  }
 }
 
