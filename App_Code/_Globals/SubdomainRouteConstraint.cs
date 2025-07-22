536dc73181987c197546b48bedc0c1ad1433c03e
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-07-22 13:16:33 +0800

    feature: subdomain-routing cci and admin-cci

---
+++ App_Code/_Globals/SubdomainRouteConstraint.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace CCIMS.Web.App_Code._Globals
+{
+    public class SubdomainRouteConstraint : IRouteConstraint
+    {
+        private readonly string[] _subdomains;
+
+        public SubdomainRouteConstraint(params string[] subdomains)
+        {
+            _subdomains = subdomains;
+        }
+
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            var host = httpContext.Request.Host.Host;
+            var parts = host.Split('.');
+            if (parts.Length < 2)
+            {
+                return false;
+            }
+            var subdomain = parts[0];
+            return _subdomains.Contains(subdomain);
+        }
+    }
+}
