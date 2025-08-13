638a585a724e254543a34319de91f35a42f1bd3a
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-08-13 15:17:30 +0800

    fix: OPS view case details from the dashboard analytics

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -8,38 +8,51 @@ using CCIMS.Web.Models.ViewModels;
 
 namespace CCIMS.Web.Controllers
 {
-  [Authorize]
-  public class DashboardController : Controller
-  {
-    private readonly ILogger<DashboardController> _logger;
-    private readonly IDashboardRepository _dashboardRepo;
-
-    public DashboardController(ILogger<DashboardController> logger, IDashboardRepository dashboardRepo)
+    [Authorize]
+    public class DashboardController : Controller
     {
-      _logger = logger;
-      _dashboardRepo = dashboardRepo;
+        private readonly ILogger<DashboardController> _logger;
+        private readonly IDashboardRepository _dashboardRepo;
+        private readonly ISecurityRepository _secureRepo;
+
+        public DashboardController(ILogger<DashboardController> logger, IDashboardRepository dashboardRepo, ISecurityRepository secureRepo)
+        {
+            _logger = logger;
+            _dashboardRepo = dashboardRepo;
+            _secureRepo = secureRepo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.AgingCasesCount = await _dashboardRepo.GetAgingCasesCountAsync();
+            ViewBag.ClosedCasesCount = await _dashboardRepo.GetClosedCasesCountAsync();
+            ViewBag.CasesCreatedToday = await _dashboardRepo.GetCasesCreatedTodayCountAsync();
+            ViewBag.TotalCasesCount = await _dashboardRepo.GetTotalCasesCountAsync();
+            ViewBag.WeeklyCasesChart = await _dashboardRepo.GetWeeklyCasesChartAsync();
+            ViewBag.WeeklyClosedCasesChart = await _dashboardRepo.GetWeeklyClosedCasesChartAsync();
+            ViewBag.Top3ServicePartnersAgingCases = await _dashboardRepo.GetTop3ServicePartnersAgingCases();
+            var latestCaseSubmission = await _dashboardRepo.GetLatestCaseSubmissionsAsync();
+            var topAgingCases = await _dashboardRepo.GetTopAgingCases();
+
+            if (User.IsInRole("OPS"))
+            {
+                foreach (var _case in latestCaseSubmission)
+                    _case.CaseNumber = await _secureRepo.EncryptIDAsync(_case.CaseNumber);
+
+                foreach (var _case in topAgingCases)
+                    _case.CaseNumber = await _secureRepo.EncryptIDAsync(_case.CaseNumber);
+            }
+
+            ViewBag.LatestCaseSubmission = latestCaseSubmission;
+            ViewBag.TopAgingCases = topAgingCases;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
-
-    public async Task<IActionResult> Index()
-    {
-      ViewBag.AgingCasesCount = await _dashboardRepo.GetAgingCasesCountAsync();
-      ViewBag.ClosedCasesCount = await _dashboardRepo.GetClosedCasesCountAsync();
-      ViewBag.CasesCreatedToday = await _dashboardRepo.GetCasesCreatedTodayCountAsync();
-      ViewBag.TotalCasesCount = await _dashboardRepo.GetTotalCasesCountAsync();
-      ViewBag.WeeklyCasesChart = await _dashboardRepo.GetWeeklyCasesChartAsync();
-      ViewBag.WeeklyClosedCasesChart = await _dashboardRepo.GetWeeklyClosedCasesChartAsync();
-      ViewBag.LatestCaseSubmission = await _dashboardRepo.GetLatestCaseSubmissionsAsync();
-      ViewBag.Top3ServicePartnersAgingCases = await _dashboardRepo.GetTop3ServicePartnersAgingCases();
-      ViewBag.TopAgingCases = await _dashboardRepo.GetTopAgingCases();
-
-
-      return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-  }
 }
