a4f44e4cbdde2e671b89174d70a33436cad1f657
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-10-15 09:53:31 +0800

    fix: limit 1500 characters in data model annotation

--- Models/ViewModels/FeedbackViewModel.cs
+++ Models/ViewModels/FeedbackViewModel.cs
@@ -1,10 +1,12 @@
-﻿namespace CCIMS.Web.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CCIMS.Web.Models.ViewModels
 {
     public class FeedbackViewModel
     {
         public string Token { get; set; }
         public string CaseNumber { get; set; }
         public int Rating { get; set; }
-        public string Comment { get; set; }
+        [MaxLength(length: 1500, ErrorMessage = "Maximum number (1500) of characters reached")] public string Comment { get; set; }
     }
 }
