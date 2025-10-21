f51b5d6ecd2c94616d0039212aabfe6cd9d245ca
Aldin Amaya <din_amaya@msi-ecs.com.ph>
2025-10-21 09:00:55 +0800

    fix: aged cases link in scriban template

--- Models/ViewModels/EmailAssetViewModel.cs
+++ Models/ViewModels/EmailAssetViewModel.cs
@@ -10,14 +10,17 @@ namespace CCIMS.Web.Models.ViewModels
         public string AdminUrl { get; }
         public string CaseNumber { get; set; }
         public string CaseTrackingLink { get; }
+        public string AdminCaseTrackingLink { get; }
         public string FeedbackFormLink { get; }
 
         public EmailAssetViewModel(IConfigurationRepository configRepo, string caseNumber, string encryptedCaseNumber)
         {
             var _uri = new Uri(configRepo.GetBaseUrl());
             BaseUrl = _uri.AbsoluteUri;
+            AdminUrl = _uri.AbsoluteUri;
             CaseNumber = caseNumber;
             CaseTrackingLink = new Uri(_uri, $"Cases/Tracking?refNo={encryptedCaseNumber}").AbsoluteUri;
+            AdminCaseTrackingLink = new Uri(_uri, $"Cases/Tracking?refNo={encryptedCaseNumber}").AbsoluteUri;
 
             FeedbackFormLink = new Uri(_uri, $"Customer/Feedback?token={encryptedCaseNumber}").AbsoluteUri;
         }
